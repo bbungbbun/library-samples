986ddeec4b8ad431b03cb3a5933d0b973eb07eab
ynnotun <flexible.yuni@gmail.com>
2023-01-18 11:04:13 +0900

    matter.js 샘플 제작

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatterJsComponent } from './matter-js/matter-js.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatterJsComponent
   ],
   imports: [
     BrowserModule,
